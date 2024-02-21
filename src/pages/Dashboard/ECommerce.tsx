146b75687950047bdfc76c381f2f2d8ee897381b
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-02-21 22:18:38 +0100

    template

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ChartOne from '../../components/Charts/ChartOne';
+import ChartThree from '../../components/Charts/ChartThree';
+import ChartTwo from '../../components/Charts/ChartTwo';
+import ChatCard from '../../components/Chat/ChatCard';
+import MapOne from '../../components/Maps/MapOne';
+import TableOne from '../../components/Tables/TableOne';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+const ECommerce: React.FC = () => {
+  return (
+    <DefaultLayout>
+      
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartOne />
+        <ChartTwo />
+        <ChartThree />
+        <MapOne />
+        <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default ECommerce;
